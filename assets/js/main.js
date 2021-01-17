f0284068a31cbc43a20b59b20b8cdf00b8dd2772
Arnold <antaloczi.arnold@gmail.com>
2021-01-17 15:03:27 +0100

    Added footer, footer design and scroll animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+// Scrool animation
+$(document).ready(function () {
+    $('a').on('click', function (event) {
+        if (this.hash !== '') {
+            event.preventDefault();
+            const hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 300, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+});
